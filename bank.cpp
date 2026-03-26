c29a5f03a8e914ee8ce7571e545d51a92d66e64a
ka-zzr27 <kawtheralmosawi904@gmail.com>
2026-03-26 20:55:21 +0300

    Update bank.cpp

--- bank.cpp
+++ bank.cpp
@@ -11,6 +11,11 @@ int main() {
     cout << "Welcome: " << accountHolder << endl;
     cout << "Your current balance is: $" << balance << endl;
     cout << "--------------------------" << endl;
-
+double deposit;
+    cout << "Enter deposit amount: ";
+    cin >> deposit;
+    balance = balance + deposit;
+    cout << "Deposit successful! New balance: $" << balance << endl;
+    
     return 0;
 }
